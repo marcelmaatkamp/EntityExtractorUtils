cf9041b6d082307b48222bbec0dfdf8550218e9e
marcel <marcel@asrock-ubuntu-01.(none)>
2011-07-06 22:03:05 +0200

    marcel

--- src/main/java/kvirc/KVIrcChatImportStarter.java
+++
@@ -1,9 +0,0 @@
-package kvirc;
-
-public class KVIrcChatImportStarter { 
-
-  public static void main(String[] args) { 
-    KVIrcChatImporter importer = new KVIrcChatImporter("src/main/resources/kvirc/query/");
-  }
-
-}
